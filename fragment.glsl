dae391e67473d1c0b928a3990228112fcffcea48
Russ <72242656+Russ741@users.noreply.github.com>
2024-01-19 18:59:08 -0500

    Prepend scaffolding to fragment program.

--- fragment.glsl
+++ fragment.glsl
@@ -1,13 +1,3 @@
-#version 100
-
-precision mediump float;
-
-void mainImage( out vec4 fragColor, in vec2 fragCoord );
-
-void main() {
-    mainImage(gl_FragColor, gl_FragCoord.xy);
-}
-
 void mainImage( out vec4 fragColor, in vec2 fragCoord ) {
     /* TODO: Provide and use iResolution rather than hardcoded numbers. */
     fragColor = vec4(fragCoord.x / 300.0, fragCoord.y / 150.0, 0.0, 1.0);
